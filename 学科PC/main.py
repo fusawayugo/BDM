401282331baa22402e224078fb5fc051362d0e00
bpbpbpb <fusawayugo0712@g.ecc.u-tokyo.ac.jp>
2024-04-16 14:37:42 +0900

    a

--- 学科PC/main.py
+++ 学科PC/main.py
@@ -56,7 +56,7 @@ async def scroll(client):
                     ms_init=0
                 else:
                     asyncio.create_task(tan_mag_shr(x,z,initial_theta))
-                await asyncio.sleep(0.1)
+                await asyncio.sleep(0.2)
             else:
                 ms_init=1
                 sc_init=1
